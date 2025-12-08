1170355baa54beddf788eb516be32285f532919e
Ozan Güneysu <ozanguneysu6@gmail.com>
2025-12-08 17:47:33 +0300

    Session Paketi Eklendi Fakat Session İşlemleri Dahaca Yapılmadı.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
-
+const session = require('express-session');
 
 const userRoutes = require('./routes/userRoutes');
 const connection = require('./dbProcesses/dbConnection');
@@ -14,6 +14,12 @@ app.use(express.json());
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(userRoutes);
 
