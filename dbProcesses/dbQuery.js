f5947ca234bb0652855af351edaba7f168e20bc2
Ozan Güneysu <ozanguneysu6@gmail.com>
2025-12-09 19:22:19 +0300

    Bugünün Harcamaları Sayfasının Ana İskeleti Ve Matematik İşlemleri Bitti. Şu an Örnek Verileri Kullanıyor.

--- dbProcesses/dbQuery.js
+++ dbProcesses/dbQuery.js
@@ -1,4 +1,6 @@
 const dbQuery = {
-    selectUsers: "SELECT * FROM users"
+    selectUsers: "SELECT * FROM users",
+    selectUsersExpensesById: "SELECT * FROM expenses WHERE user_id = ?",
+    selectUsersIncomeById: "SELECT * FROM income WHERE user_id = ?"
 };
 module.exports = dbQuery;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,29 +1,82 @@
 const express = require('express');
 const router = express.Router();
 const path = require("path");
-const fs = require("fs"); 
+const fs = require("fs");
 
 const navbarLinks = [
-    {id: 1, name: "Ana Sayfa", path: "/" },
-    {id: 2, name: "Bugün", path: "/today" },
-    {id: 3, name: "Bu Ay", path: "/thismonth" },
-    {id: 4, name: "Bu Yıl", path: "/thisyear" },
+    { id: 1, name: "Ana Sayfa", path: "/" },
+    { id: 2, name: "Bugün", path: "/today" },
+    { id: 3, name: "Bu Ay", path: "/thismonth" },
+    { id: 4, name: "Bu Yıl", path: "/thisyear" },
+];
+const expenses = [
+    {
+        id: 1,
+        userId: 1,
+        type: "expense",
+        amount: 400,
+        description: "Sigara",
+        date: "2025-12-09"
+    },
+    {
+        id: 2,
+        userId: 1,
+        type: "expense",
+        amount: 100,
+        description: "Yemek",
+        date: "2025-12-09"
+    },
+    {
+        id: 3,
+        userId: 1,
+        type: "expense",
+        amount: 250,
+        description: "Yemek",
+        date: "2025-12-09"
+    },
+    {
+        id: 4,
+        userId: 1,
+        type: "expense",
+        amount: 250,
+        description: "Yemek",
+        date: "2025-12-09"
+    }
+];
+
+const incomes = [
+    {
+        id: 1,
+        userId: 1,
+        type: "income",
+        amount: 1500,
+        description: "İş",
+        date: "2025-12-09"
+    },
+    {
+        id: 2,
+        userId: 1,
+        type: "income",
+        amount: 500,
+        description: "Freelance",
+        date: "2025-12-09"
+    }
 ];
 
 router.get("/", (req, res) => {
-    res.render('../views/userViews/homepage.ejs', { navbarLinks }); 
+    res.render('../views/userViews/homepage.ejs', { navbarLinks, expenses, incomes });
 });
 
 router.get("/today", (req, res) => {
-    res.render('../views/userViews/today.ejs', { navbarLinks });
+    res.render('../views/userViews/today.ejs', { navbarLinks, expenses, incomes });
 });
 
-router.get("/thismonth", (req, res) => { 
-    res.render('../views/userViews/thismonth.ejs', { navbarLinks }); 
+router.get("/thismonth", (req, res) => {
+    res.render('../views/userViews/thismonth.ejs', { navbarLinks, expenses, incomes });
 });
 
 router.get("/thisyear", (req, res) => {
-    res.render('../views/userViews/thisyear.ejs', { navbarLinks });
+    res.render('../views/userViews/thisyear.ejs', { navbarLinks, expenses, incomes });
 });
 
 module.exports = router;
